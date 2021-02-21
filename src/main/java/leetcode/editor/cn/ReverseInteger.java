74ecfd266f459129ed123e380d53b9047a8a7010
ylpanda <aukovyps@163.com>
2021-02-21 21:31:04 +0800

    leetcode 做题记录 2021-02-21

---
+++ src/main/java/leetcode/editor/cn/ReverseInteger.java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+/**
+ *给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *注意：
+ * <p>
+ *假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231, 231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * <p>
+ *示例 1：
+ * <p>
+ *输入：x = 123
+ *输出：321
+ * <p>
+ *
+ 示例 2：
+ * <p>
+ *
+ *输入：x = -123
+ *输出：-321
+ * <p>
+ *
+ *示例 3：
+ * <p>
+ *
+ *输入：x = 120
+ *输出：21
+ * <p>
+ *
+ 示例 4：
+ * <p>
+ *
+ *输入：x = 0
+ *输出：0
+ * <p>
+ *
+ *提示：
+ * <p>
+ *-231 <= x <= 231 - 1
+ * <p>
+ *Related Topics 数学
+ *👍 2465 👎 0
+*/
+
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Solution solution = new ReverseInteger().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int reverse(int x) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
